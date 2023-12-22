081d979da3ddc3f03f728aa0d4c0610e0b13f610
coffm049 <coffm049@umn.edu>
2023-12-22 13:53:08 -0600

    Removed haskel and rust language featuers

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -19,37 +19,10 @@ return {
           }
       },
 
-      -- optional
-      -- { 'quarto-dev/quarto-vim',
-      --   ft = 'quarto',
-      --   dependencies = { 'vim-pandoc/vim-pandoc-syntax' },
-      --   -- note: needs additional syntax highlighting enabled for markdown
-      --   --       in `nvim-treesitter`
-      --   config = function()
-      -- conceal can be tricky because both
-      -- the treesitter highlighting and the
-      -- regex vim syntax files can define conceals
-      --
-      -- -- see `:h conceallevel`
-      -- vim.opt.conceallevel = 1
-      --
-      -- -- disable conceal in markdown/quarto
-      -- vim.g['pandoc#syntax#conceal#use'] = false
-      --
-      -- -- embeds are already handled by treesitter injectons
-      -- vim.g['pandoc#syntax#codeblocks#embeds#use'] = false
-      -- vim.g['pandoc#syntax#conceal#blacklist'] = { 'codeblock_delim', 'codeblock_start' }
-      --
-      -- -- but allow some types of conceal in math regions:
-      -- -- see `:h g:tex_conceal`
-      -- vim.g['tex_conceal'] = 'gm'
-      -- --   end
-      -- },
-
     },
     opts = {
       lspFeatures = {
-        languages = { 'r', 'python', 'julia', 'bash', 'lua', 'html' },
+        languages = { 'r', 'python', 'julia', 'bash', 'lua', 'html', 'quarto', 'markdown' },
       },
     }
   },
@@ -363,26 +336,6 @@ return {
         filetypes = { 'sh', 'bash' }
       }
 
-      -- Add additional languages here.
-      -- See `:h lspconfig-all` for the configuration.
-      -- Like e.g. Haskell:
-      lspconfig.hls.setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = lsp_flags
-      }
-
-      lspconfig.rust_analyzer.setup{
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          ['rust-analyzer'] = {
-            diagnostics = {
-              enable = false;
-            }
-          }
-        }
-      }
 
     end
   },
